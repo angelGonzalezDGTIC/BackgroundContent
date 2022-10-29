8e30b7264c4f543939e63a68aa923e19ad17355f
Ángel González <jan.zelaznog@gmail.com>
2022-10-29 14:06:14 -0500

    Código de la sesión 2

---
+++ BackgroundContent/TableViewController.swift
@@ -0,0 +1,89 @@
+//
+//  TableViewController.swift
+//  BackgroundContent
+//
+//  Created by Ángel González on 29/10/22.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+    var personajes = [Result]()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if let url=URL(string: "https://rickandmortyapi.com/api/character/") {
+            do {
+                let bytes = try Data(contentsOf: url)
+                let rick = try JSONDecoder().decode(Rick.self, from: bytes)
+                personajes = rick.results
+            }
+            catch {
+            }
+        }
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return personajes.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "personCell", for: indexPath)
+        let personaje = personajes[indexPath.row]
+        cell.textLabel?.text = personaje.name
+
+        return cell
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- BackgroundContent/ViewController.swift
+++ BackgroundContent/ViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
@@ -14,6 +15,33 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let configuracion = URLSessionConfiguration.default
+        let sesion = URLSession(configuration: configuracion)
+        guard let laURL = URL(string:"https://www.nasa.gov/sites/default/files/thumbnails/image/halloween_sun_2014.jpeg")
+        else { return }
+        
+        let request = URLRequest(url: laURL)
+        let tarea = sesion.dataTask(with:request) { datos, response, error in
+            if  nil != error {
+                print ("algo salió mal \(String(describing: error?.localizedDescription))")
+                return
+            }
+            guard let bytes = datos else {
+                print ("el response no trajo datos")
+                return
+            }
+            // para cualquier cambio que se haga en UI debe ser en el thread principal
+            DispatchQueue.main.async {
+                let imageview = UIImageView(frame: self.view.frame)
+                imageview.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+                imageview.image = UIImage(data:bytes)
+                self.view.addSubview(imageview)
+            }
+        }
+        tarea.resume()
+    }
 }
 
+    
